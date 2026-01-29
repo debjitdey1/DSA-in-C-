895c71bb30914b3eac1901fe0bc08b2f22c9750d
Debjit Dey <121121842+debjitdey1@users.noreply.github.com>
2026-01-29 19:50:01 +0530

    Add files via upload

---
+++ Second.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the First Number:";
+    cin >> a;
+    cout<<"Enter the Second Number:";
+    cin >> b;
+    cout<<"Enter the TOTAL number: "<< a+b;
+
+};
